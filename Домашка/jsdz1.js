e24121e24210b4b9092ac2cc1ebc954655961604
Ramis <ramis.a9@mail.ru>
2023-07-19 17:49:20 +0400

    Актуальная версия

--- Домашка/jsdz1.js
+++ Домашка/jsdz1.js
@@ -1,22 +1,24 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц","30 000");
-let time = prompt("Введите дату в формате YYYY-MM-DD","1996-05-08");
-console.log(money, time);
+let money = prompt("Ваш бюджет на месяц"," Пример: 30000");
+let time = prompt("Введите дату в формате YYYY-MM-DD","Пример: 1996-05-08");
 
-let exp1 = prompt("Введите обязательную статью расходов в этом месяце");
-let exp2 = prompt("Во сколько обойдется?");
+let exp1 = prompt("Введите обязательную статью расходов в этом месяце через запятую");
+let exp2 = prompt("Во сколько обойдется?","Пример: 20000");
 let expensens = {
-    whatMust(prompt("Введите обязательную статью расходов в этом месяце")):howMach(prompt("Во сколько обойдется?"))
-},
+    whatMust:exp1,
+    howMach:exp2
+};
 
 let appData = {
     cash:money,
     timeData:time,
-    
-    optionalExpenses:"empty",
-    income:"arr",
+    expensess:expensens,
+    optionalExpenses:"",
+    income:"",
     savings:false
 };
-console.log(appData)
 
+alert("После покупки" + " " + expensens.whatMust + " " + "свободных денег у тебя останется" + " " + (money-exp2));
+console.log("Существует типы - строка, число, логическое выражение, undefind, null, объект и символ - всего 7");
+console.log("|| - или, && - ""и"" операторы сравнения, логические выражения.")
