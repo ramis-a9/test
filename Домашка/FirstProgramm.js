fb04325a772102bcf577f8a8bfcb7dd4935552fd
Ramis <ramis.a9@mail.ru>
2023-08-01 17:53:43 +0400

    Актуальная версия

--- Домашка/FirstProgramm.js
+++ Домашка/FirstProgramm.js
@@ -1,15 +1,21 @@
 'use strict';
 
+alert("Добро пожаловать на страницу с расчетом Вашего бюджета. Страница максимально бесполезная и безопасная, но за то сможете поиграть с числами и наглядно посмотреть Ваш бюджет. Эта программа работает в режиме тестирования. Все ошибки вы можете присылать на Вотсапп - 79270733739.");
+
+let iq = prompt("Внимание введите любую букву или цифры. Если Ваш IQ меньше 10 - введите цифру 1");
+if (iq == 1) {
+    alert("Хах - ЛОХ");
+};
+
 let money, time;
 
 function start () {
     while (isNaN(money) || money == "" || money == null || money == NaN) {
         money = +prompt("Ваш бюджет на месяц"," Пример: 30000").replace(/[^\d]/g, '');
 };
-    time = prompt("Введите дату в формате YYYY-MM-DD","Пример: 2023-31-07");
+    time = prompt("Введите сегодняшнюю дату в формате YYYY-MM-DD","Пример: 2023-31-07");
 };
 start();
-
 
 let appData = {
     cash:money,
@@ -19,8 +25,8 @@ let appData = {
     income:[],
     savings:{},
     chooseExpenses: function () {
-        for (let i = 0; i < 2; i++) {
-            let exp1 = prompt("Введите обязательную статью расходов в этом месяце через запятую");
+        for (let i = 0; i < 1; i++) {
+            let exp1 = prompt("Введите обязательную статью расходов в этом месяце текстом, через запятую");
             let exp2 = +prompt("Во сколько обойдется?","Пример: 20000").replace(/[^\d]/g, '');
             if ( (typeof(exp1)) === "string" && (typeof(exp1)) != null && (typeof(exp2)) != null
                 && exp1 != "" && exp2 != "" && exp1.length < 50) {
@@ -33,17 +39,17 @@ let appData = {
         
     },
     detectDayBudget: function () {
-        appData.moneyPerDay = (appData.cash / 30).toFixed(); //он округлит до целого, если вписать в скобки число, то до этого числа после запятой. НО он в любом случае даст строковое значение!
+        appData.moneyPerDay = ((appData.cash+appData.monthIncome)/ 30).toFixed(); //он округлит до целого, если вписать в скобки число, то до этого числа после запятой. НО он в любом случае даст строковое значение!
         alert("Ежедневный бюджет " + appData.moneyPerDay);
     },
     detectLevel: function () {
-        if (money <= 10000) {
-            alert ("Так как Ваш бюджет меньше " + money + "вы относитесь к населению с низким уровнем дохода");
+        if ((Number(money)+Number(appData.monthIncome)) <= 10000) {
+            alert ("Так как Ваш ежемесячный бюджет с доходом от банка меньше " + (Number(money)+Number(appData.monthIncome)) + " вы относитесь к населению с низким уровнем дохода");
         } else {
-            if (money >= 100000) {
-            alert ("Так как Ваш бюджет больше " + money + "вы относитесь к населению с высоким уровнем дохода");
+            if ((Number(money)+Number(appData.monthIncome)) >= 100000) {
+            alert ("Так как Ваш ежемесячный бюджет с доходом от банка больше " + (Number(money)+Number(appData.monthIncome)) + " вы относитесь к населению с высоким уровнем дохода");
             } else {
-            alert ("Так как Ваш бюджет до 100 000 руб, вы относитесь к населению со средним уровнем дохода");
+            alert ("Так как Ваш ежемесячный бюджет с доходом от банка до 100 000 руб, вы относитесь к населению со средним уровнем дохода");
             }
         }
     },
@@ -51,15 +57,17 @@ let appData = {
         appData.savings = confirm("Eсть ли у Вас накопления в банке под процент?");
         if (appData.savings == true) {
             let save = +prompt("Какова Сумма накоплений"),
-                percent = +prompt("Под какой процент?");
-            appData.monthIncome = (save/100/12*percent).toFixed();
-            appData.monthIncome = +(appData.monthIncome);
+                percent = +prompt("Под какой процент?").replace(/,/, ".");
+            appData.monthIncome = (((save/100/12)*(percent)).toFixed());
+            console.log(appData.monthIncome+'!!!!!!!!!!!!!!!!!!!!!');
+            appData.monthIncome = Number(appData.monthIncome);
             alert("Доход в месяц с Вашего депозита " + appData.monthIncome);
+            console.log(appData.monthIncome+'!!!!!!!!!!!!!!!!!!!!!');
         }
     },
     chooseOptExpenses: function() {
-        for (i = 1; i < 3; i++) {
-            let opt = prompt("Введите статья необязательных расходов","Пример: Одежда, отдых, гуляш мясной");
+        for (let i = 1; i < 2; i++) {
+            let opt = prompt("Введите статью необязательных расходов","Пример: Одежда, отдых, гуляш мясной");
             appData.optionalExpenses[i] = opt;
         }
     },
@@ -76,6 +84,13 @@ let appData = {
         })
     }
 };
+appData.chooseExpenses();
+appData.chooseOptExpenses();
+appData.checkSavings();
+appData.chooseIncomeFunc();
+appData.detectDayBudget();
+appData.detectLevel();
+
 
 
 for (let key in appData) {
@@ -85,8 +100,3 @@ for (let key in appData) {
 console.log("Существует типы - строка, число, логическое выражение, undefind, null, объект и символ - всего 7");
 console.log("|| - или, && - И .  операторы сравнения, логические выражения.");
 console.log('Перебрать свойства объекта можно через метод for in');
-
-
-
-
-
