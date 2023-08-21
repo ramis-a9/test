03ea9cd2c1fa63243b33fba7fa1340306091a993
Ramis <ramis.a9@mail.ru>
2023-08-21 17:53:44 +0400

    Актуальная версия

--- Домашка/Работа с объектами/Для ДЗ №5/js/script.js
+++ Домашка/Работа с объектами/Для ДЗ №5/js/script.js
@@ -1,23 +1,20 @@
 'use strict';
 
-let backgroundBody = function() {
-    document.body.style.backgroundImage = 'url(img/apple_true.jpg)';
-    };
-backgroundBody();
-
 let menuItem = document.querySelectorAll('.menu-item'),
     menuClass = document.querySelector('.menu'),
     titleId = document.getElementById('title'),
     advDelete = document.querySelector('.adv'),
     promptId = document.getElementById('prompt');
+    
 
 menuClass.insertBefore(menuItem[2], menuItem[1]);
 titleId.textContent ='Мы продаем только подлинную технику Apple';
 advDelete.remove();
-promptId.textContent = prompt('Как вы относитесь к тенхнике Apple?');
+// promptId.textContent = prompt('Как вы относитесь к тенхнике Apple?');
 
 console.log(menuClass);
 console.log(titleId);
 console.log(advDelete);
 console.log(promptId);
+
 
