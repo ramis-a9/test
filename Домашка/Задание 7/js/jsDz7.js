17705257a29e359534e1b964f1ac058ceaf90459
Ramis <ramis.a9@mail.ru>
2023-08-29 17:50:19 +0400

    Актуальная версия

---
+++ Домашка/Задание 7/js/jsDz7.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let bntStart = document.getElementById('start'),
+    valueBlock = document.getElementsByTagName('value'),
+    inputBlocks = document.querySelectorAll('input','expenses-item');
+
+console.log(inputBlocks);
+
