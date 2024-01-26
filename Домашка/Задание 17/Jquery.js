82d0768d26ce939c1fab2f77eff935ccfcb971fb
Ramis <ramis.a9@mail.ru>
2024-01-26 17:53:22 +0400

    Актуальная версия

--- Домашка/Задание 17/Jquery.js
+++ Домашка/Задание 17/Jquery.js
@@ -1,23 +1,27 @@
 $(document).ready(function(){
     
+    // Обращаться к css для добавления dysplay
+
     $('.main_btna').on('click', function() {
-        $('.overlay').fadeToggle('slow').css('display','block')
-        $('.modal').css('display','block');
+        $('.overlay').fadeToggle('slow');
+        $('.modal').slideDown(700);
     }); // Выбрать тур
 
-    // $('window').on('click', function() {
-    //     if($('.overlay').css('display' == 'block')) {
-    //         console.log('1');
-    //     }
-    // });
-
     $(".main_btn").on('click', function() {
-        console.log('Вы кликнули на получить консультацию');
+        $('.overlay').fadeToggle('slow');
+        $('.modal').slideDown(700);
     }); // Получить консультация
 
     $('a[href|="#sheldure"]').on('click', function() {    //обращение к href !! очень полезный лайфхак!
-        console.log('Вы кликнули на расписание туров');
+        $('.overlay').fadeToggle('slow');
+        $('.modal').slideDown(700);
     }); // Расписание туров
+
+
+    $('span:eq(0)').on('click', function() {
+        $('.overlay').fadeToggle('slow')
+        $('.modal').slideUp(700);
+    });
 
 
 
